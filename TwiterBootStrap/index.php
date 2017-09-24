8d74aa1fd31399a5e5eaeeb3a4526331f6d1dd7d
lyg0114 <whdnseowkd@naver.com>
2017-09-24 19:57:12 +0900

    BootStrap Complete
    ㅇㅋ

--- TwiterBootStrap/index.php
+++ TwiterBootStrap/index.php
@@ -10,21 +10,24 @@ $result = mysqli_query($conn, "SELECT * FROM topic");
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-  <link rel="stylesheet" type="text/css" href="http://localhost:8080/TwiterBootStrap/style.css">
-  <link href="http://localhost:8080/bootstrap-3.3.4-dist/css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="http://localhost:8080/TwiterBootStrap/style.css">
+<link href="http://localhost:8080/bootstrap-3.3.4-dist/css/bootstrap.min.css" rel="stylesheet">
+
 
   </head>
 
 <body id="target">
-    <header>
-    <img src="https://s3.ap-northeast-2.amazonaws.com/opentutorials-user-file/course/94.png" alt="생활코딩">
+  <div class="container-flu">
+
+    <header class="jumbotron text-center">
+    <img src="https://s3.ap-northeast-2.amazonaws.com/opentutorials-user-file/course/94.png" alt="생활코딩" class="img-circle" id="logo" >
         <h1><a href="http://localhost:8080/TwiterBootStrap/index.php">JavaScript</a></h1>
   </header>
 
   <div class="row">
 
     <nav class="col-md-3">
-        <ol>
+        <ol class="nav nav-pills nav-stacked">
     <?php
     while( $row = mysqli_fetch_assoc($result)){
       echo '<li><a href="http://localhost:8080/TwiterBootStrap/index.php?id='.$row['id'].'">'.$row['title'].'</a></li>'."\n";
@@ -34,11 +37,6 @@ $result = mysqli_query($conn, "SELECT * FROM topic");
     </nav>
 
 <div class="col-md-9">
-  <div id="control">
-      <input type="button" value="white" onclick="document.getElementById('target').className='white'"/>
-      <input type="button" value="black" onclick="document.getElementById('target').className='black'" />
-      <a href="http://localhost:8080/TwiterBootStrap/write.php">쓰기</a>
-    </div>
 
     <article>
     <?php
@@ -52,10 +50,22 @@ $result = mysqli_query($conn, "SELECT * FROM topic");
     ?>
 
     </article>
+    <hr>
+    <div id="control">
+      <div class="btn-group" roel="group" aria-laber="...">
+        <input type="button" value="white" class ="btn btn-default btn-lg" onclick="document.getElementById('target').className='white'"/>
+        <input type="button" value="black" class ="btn btn-default btn-lg" onclick="document.getElementById('target').className='black'" />
+      </div>
+        <a href="http://localhost:8080/TwiterBootStrap/write.php" class ="btn btn-success btn-lg">쓰기</a>
+      </div>
+
 
 </div>
 
 
+
+
+  </div>
 
 
   </div>
--- TwiterBootStrap/write.php
+++ TwiterBootStrap/write.php
@@ -6,42 +6,75 @@ $result = mysqli_query($conn, "SELECT * FROM topic");
 <!DOCTYPE html>
 <html>
 <head>
-     <meta charset="utf-8">
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+  <link href="http://localhost:8080/bootstrap-3.3.4-dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="http://localhost:8080/TwiterBootStrap/style.css">
 </head>
+
 <body id="target">
-    <header>
-    <img src="https://s3.ap-northeast-2.amazonaws.com/opentutorials-user-file/course/94.png" alt="생활코딩">
+  <div class="container-flu">
+
+    <header class="jumbotron text-center">
+    <img src="https://s3.ap-northeast-2.amazonaws.com/opentutorials-user-file/course/94.png" alt="생활코딩" class="img-circle" id="logo" >
         <h1><a href="http://localhost:8080/TwiterBootStrap/index.php">JavaScript</a></h1>
   </header>
-    <nav>
-        <ol>
+
+  <div class="row">
+
+    <nav class="col-md-3">
+        <ol class="nav nav-pills nav-stacked">
     <?php
     while( $row = mysqli_fetch_assoc($result)){
-      echo '<li><a href="http://localhost:8080/TwiterBootStrap/index.php'.$row['id'].'">'.$row['title'].'</a></li>'."\n";
+      echo '<li><a href="http://localhost:8080/TwiterBootStrap/index.php?id='.$row['id'].'">'.$row['title'].'</a></li>'."\n";
     }
     ?>
         </ol>
     </nav>
-  <div id="control">
-    <input type="button" value="white" onclick="document.getElementById('target').className='white'"/>
-    <input type="button" value="black" onclick="document.getElementById('target').className='black'" />
-    <a href="http://localhost:8080/TwiterBootStrap/write.php">쓰기</a>
-  </div>
 
-  <article>
-    <form action="http://localhost:8080/TwiterBootStrap/process.php" method="post">
-      <p>
-        제목 : <input type="text" name="title">
-      </p>
-      <p>
-        작성자 : <input type="text" name="author">
-      </p>
-      <p>
-        본문 : <textarea name="description"></textarea>
-      </p>
-      <input type="submit" name="name">
-    </form>
-  </article>
+<div class="col-md-9">
+
+    <article>
+      <form action="http://localhost:8080/TwiterBootStrap/process.php" method="post">
+
+        <div class="form-group">
+          <label for="form-title">제목</label>
+          <input type="text" class="form-control" name="title" id="form-title" placeholder="제목을 적어주세요.">
+        </div>
+
+        <div class="form-group">
+          <label for="form-author">작성자</label>
+          <input type="text" class="form-control" name="author" id="form-author" placeholder="작성자를 적어주세요.">
+        </div>
+
+        <div class="form-group">
+          <label for="form-description">본문</label>
+          <textarea class="form-control" rows="10" name="description"  id="form-description" placeholder="본문을 적어주세요."></textarea>
+        </div>
+
+        <input type="submit" name="name" class ="btn btn-default btn-lg">
+      </form></article>
+    <hr>
+    <div id="control">
+      <div class="btn-group" roel="group" aria-laber="...">
+        <input type="button" value="white" class ="btn btn-default btn-lg" onclick="document.getElementById('target').className='white'"/>
+        <input type="button" value="black" class ="btn btn-default btn-lg" onclick="document.getElementById('target').className='black'" />
+      </div>
+
+        <a href="http://localhost:8080/TwiterBootStrap/write.php" class ="btn btn-success btn-lg">쓰기</a>
+
+      </div>
+    </div>
+  </div>
+</div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="http://localhost:8080/bootstrap-3.3.4-dist/css/bootstrap.min.js"></script>
+
 </body>
 </html>
