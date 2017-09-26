89233ec4636234f69b9756dc28a30633fde18694
lyg0114 <whdnseowkd@naver.com>
2017-09-26 10:50:44 +0900

    모달테스트 성공
    ㅇㅋ

--- MusicAcademy/cal/cal.php
+++ MusicAcademy/cal/cal.php
@@ -9,10 +9,10 @@ if(!isset($_SESSION['is_login'])){
 <html xmlns="http://www.w3.org/1999/xhtml" lang="ko" xml:lang="ko">
 <head>
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-  <link rel="stylesheet" type="text/css" href="http://localhost:8080/MusicAcademy/cal/cal.css">
   <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-  <link rel="stylesheet" type="text/css" href="http://localhost:8080/TwiterBootStrap/style.css">
   <link href="http://localhost:8080/bootstrap-3.3.4-dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="http://localhost:8080/MusicAcademy/cal/cal.css">
+
 </head>
 
 <body id="target">
@@ -60,41 +60,47 @@ else{
       print '<li style="display:none" class="DBtime">'.$row['time'].'</li>';
   }
   ?>
-<nav id="basicInfo">
-  <p> 시간 예약하기 </p>
-  <form name = "success" method="post" id="send">
-    <p> 년 : <input type="text" name ='year' id="year" value = "2017"/></p>
-    <p> 월 : <select  id="month"  name= 'month'>
-      <option calss ="c" value="1">1</option>
-      <option calss ="c" value="2">2</option>
-      <option calss ="c" value="3">3</option>
-      <option calss ="c" value="4">4</option>
-      <option calss ="c" value="5">5</option>
-      <option calss ="c" value="6">6</option>
-      <option calss ="c" value="7">7</option>
-      <option calss ="c" value="8">8</option>
-      <option calss ="c" value="9" >9</option>
-      <option calss ="c" value="10">10</option>
-      <option calss ="c" value="11">11</option>
-      <option calss ="c" value="12">12</option>
-    </select></p>
-    <p id="show"> </p>
-    <p> 선택날짜 : <input type="text" id="searchday" name="searchday" value = "10"/></p>
-    <p> 선택시간 : <select id="sele" name="reserVationTime">
-      <option id="six" value="6">6:00</option>
-      <option id="seven" value="7">7:00</option>
-      <option id="eight" value="8">8:00</option>
-    </select>
-  </p>
-<input id ="mon" type="button" value="날짜확인" onclick= 'mySubmit(1)'/>
-  <input id ="res" type="button" value="예약하기" onclick= 'mySubmit(2)'/>
-  <input id ="del" type="button" value="예약취소" onclick= 'mySubmit(3)'/>
 
-  </form>
-</nav>
+  <div class="row">
 
-<h3 id = "title"></h3>
-<div id="kCalendar"></div>
+    <nav id="basicInfo" class="col-md-3">
+      <p> 시간 예약하기 </p>
+      <form name = "success" method="post" id="send">
+        <p> 년 : <input type="text" name ='year' id="year" value = "2017"/></p>
+        <p> 월 : <select  id="month"  name= 'month'>
+          <option calss ="c" value="1">1</option>
+          <option calss ="c" value="2">2</option>
+          <option calss ="c" value="3">3</option>
+          <option calss ="c" value="4">4</option>
+          <option calss ="c" value="5">5</option>
+          <option calss ="c" value="6">6</option>
+          <option calss ="c" value="7">7</option>
+          <option calss ="c" value="8">8</option>
+          <option calss ="c" value="9" >9</option>
+          <option calss ="c" value="10">10</option>
+          <option calss ="c" value="11">11</option>
+          <option calss ="c" value="12">12</option>
+        </select></p>
+        <p id="show"> </p>
+        <p> 선택날짜 : <input type="text" id="searchday" name="searchday" value = "10"/></p>
+        <p> 선택시간 : <select id="sele" name="reserVationTime">
+          <option id="six" value="6">6:00</option>
+          <option id="seven" value="7">7:00</option>
+          <option id="eight" value="8">8:00</option>
+        </select>
+      </p>
+    <input id ="mon" type="button" value="날짜확인" onclick= 'mySubmit(1)'/>
+      <input id ="res" type="button" value="예약하기" onclick= 'mySubmit(2)'/>
+      <input id ="del" type="button" value="예약취소" onclick= 'mySubmit(3)'/>
+
+      </form>
+    </nav>
+        <h3 id = "title"></h3>
+    <div id="calinfo" class="col-md-7" >
+      <div id="kCalendar"></div>
+    </div>
+
+  </div>
 
 
 <script type="text/javascript" src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
